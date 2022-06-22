dc81bf8db2e0a617363326abd1bfa83d8dcb9708
initial@gmail.com <teamdailypractice@gmail.com>
2022-06-22 16:50:55 +0800

    Dropdown selection - Group

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -5,11 +5,12 @@ import { FaArrowRight } from "react-icons/fa"
 export default function BookablesList() {
 
     const [bookableIndex, setBookableIndex] = useState(1);
+    const [group, setGroup] = useState("Kit");
 
-    const group = "Rooms";
+
     const bookables = data.bookables;
     const bookablesInGroup = bookables.filter(b => b.group === group);
-
+    const groups = [...new Set(bookables.map(b => b.group))]
 
 
     function changeBookable(selectedIndex) {
@@ -18,9 +19,15 @@ export default function BookablesList() {
     }
     function nextBookable() {
         setBookableIndex((bookableIndex + 1) % bookablesInGroup.length);
-    } 
+    }
     return (
         <div>
+            <select
+                value={group}
+                onChange={(e) => setGroup(e.target.value)}>
+                {groups.map(g => <option key={g} value={g}>{g}</option>)}
+
+            </select>
             <ul className="bookables items-list-nav">
                 {bookablesInGroup.map((b, i) => {
                     return (

385e96ae53cde4af63392abc8639f5e693d26dae
initial@gmail.com <teamdailypractice@gmail.com>
2022-06-23 17:01:48 +0800

    UserPicker - Top menu - incomplete

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,19 +1,12 @@
-import React, { useState } from "react";
 import data from "../../static.json";
 
 export default function UserPicker() {
-
-    const [user, setUser] = useState("Mark");
-
     const users = data.users;
 
     return (
-        <select
-            value={user}
-            onChange={(e) => setUser(e.target.value)}
-        >
-            {users.map((u, index) =>
-                <option key={index} value={u.name}>{u.name}</option>
+        <select>
+            {users.map((u) =>
+                <option key={u.id}>{u.name}</option>
             )}
 
         </select>
